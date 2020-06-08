3c3c6c7e5fb46d08500bcb1f53be8688b011fb39
Ben Gardiner <bennygards@gmail.com>
2020-06-08 20:13:47 +1000

    inital commit - choosing classes

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bingo_Number_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class BingoDraw {
+        public int upperlimit;
+        public List<int> List;
+
+        Random rand = new Random();
+
+    }
+
+
+
+
+
+
+
+
+}
