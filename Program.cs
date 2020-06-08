15ad97bf759e38cf6e046f63def4c7f35c2b7a8a
Ben Gardiner <bennygards@gmail.com>
2020-06-08 20:34:44 +1000

    adding the rand for the draw

--- Program.cs
+++ Program.cs
@@ -11,11 +11,41 @@ namespace Bingo_Number_Generator
         }
     }
 
-    class BingoDraw {
+    class Draw {
+        public int min;
         public int upperlimit;
-        public List<int> List;
+        public List<int> DrawStorage;
 
         Random rand = new Random();
+
+        public Draw(int upperlimit)
+        {
+
+            this.upperlimit = upperlimit;
+            this.DrawStorage = new List<int>();
+
+        }
+
+        public void DrawNextNum(){
+            bool validNum = false;
+            var randnum = rand.Next(1, this.upperlimit);
+            while (validNum == false)
+
+
+
+
+            return randnum;
+        }
+        
+
+    }
+
+    class Interface {
+
+
+
+
+
 
     }
 
