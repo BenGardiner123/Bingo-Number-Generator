29df21f59c963bb496f4c02e6ce6c372d8a45af5
Ben Gardiner <bennygards@gmail.com>
2020-06-10 00:30:14 +1000

    added more to the ligc - hopefulyl workign

--- Draw.cs
+++ Draw.cs
@@ -11,10 +11,28 @@ namespace Bingo_Number_Generator
 
         Random rand = new Random();
 
-        public Draw(int limit)
+        public Draw()
         {
-            this.upperlimit = limit;
+            
             this.DrawStorage = new List<int>();
+
+        }
+         public void mainMenu()
+        {  
+            Console.WriteLine("Welcome to the Swinburne Bingo Club");
+            Console.WriteLine("1. Draw next number");
+            Console.WriteLine("2. View all drawn numbers");
+            Console.WriteLine("3. Check specific number");
+            Console.WriteLine("4. Enter a list of numbers to check against drawn list");
+            Console.WriteLine("5. Exit");
+        }
+        public void viewAllDrawnnums()
+        {
+
+            //console writlien 1 option as they were drawn
+            this.DrawStorage.ForEach(Console.WriteLine);
+            ///option two they get srtoed into numercial order - might have to make a new list to copy them and sort them
+
 
         }
 
@@ -40,6 +58,108 @@ namespace Bingo_Number_Generator
             }
 
             DrawStorage.Add(randnum);
+
+        }
+
+        public void getupperLimit()
+        {
+
+            bool goOn = false;
+            while (!goOn)
+            {
+                System.Console.WriteLine("Please enter an upper limit for your bingo # range");
+                var userInput = Console.ReadLine();
+                var userInput_1 = Convert.ToInt32(userInput);
+                
+                int result = 0;
+                if (int.TryParse(userInput, out result))
+                {
+                
+                Console.WriteLine(result + " .... really?" + " Only Numbers Please... try again");
+                continue;
+
+                }
+
+                if (userInput_1 <= 0)
+                {
+                    Console.WriteLine(result + " .... really?" + " Only Positive Numbers Please .. try again");
+                    continue;
+                }
+                
+                else
+                {
+                    System.Console.WriteLine("You have set your upper limit");
+                    userInput_1 = this.upperlimit;
+
+                    goOn = true;
+                       
+                }
+
+            }
+            
+            mainMenu();
+
+        }
+
+        public void checkNuminList()
+        {
+
+            Console.WriteLine("Enter your number to check if it has been drawn..");
+            string targetInt = Console.ReadLine();
+            int number;
+            if (Int32.TryParse(targetInt, out number))
+            {
+                bool keepGoing = true;
+                while (keepGoing)
+                {
+                
+                    int newInt = Convert.ToInt32(targetInt);// now that im past the sentinel value need to work with an int
+                    int index = DrawStorage.IndexOf(newInt); ///this checks ans sees if the targetint is in the list
+                    if (index >= 0)//if it is there it reurns a "1"
+                    {
+                        Console.WriteLine(newInt + " Has been located, and i can confirm it has been drawn previously");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number has not been drawn - its fresh");
+
+                    }
+                    
+                    bool validInput = false;
+                    
+                    while (!validInput)
+                    {
+                        Console.WriteLine("\nTo check another number press 1 ");
+                        Console.WriteLine(" Otherwise press 2 to return to main menu");
+                        /////needs ome validation here (loop)
+                        var userChoice = Console.Read();///if i use the .read here it gets the ASCI value for that character - so this way it it has
+                        if (userChoice == 49) /// using 
+                        {
+                            keepGoing = true;
+                            validInput = true;
+                        }
+                        else if (userChoice == 50)
+                        {
+                            keepGoing = false;
+                            validInput = true;
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("invalid input");///need to then return it to the top of the list
+                        }
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("You failed to enter a readable selection - please try again");
+            }    
+            
+
+
 
         }
 
--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace Bingo_Number_Generator
     {
         static void Main(string[] args)
         {
+    
+
+            Draw d1 = new Draw();
+            d1.getupperLimit();
             
-           /*  System.Console.WriteLine("Please enter suer upper limit");
-            var userInt = 99;
-            Draw d1 = new Draw(userInt);
             d1.DrawNextNum();
             d1.DrawNextNum();
             d1.DrawNextNum();
@@ -19,20 +20,11 @@ namespace Bingo_Number_Generator
             d1.DrawNextNum();
             d1.DrawNextNum();
             d1.DrawNextNum();
+
             ///d1.DrawStorage.ForEach(Console.WriteLine);//this is a really cool way of doing it because this methid chooses the right type for the list.
-            Interface i1 = new Interface();
-            i1.startMenu();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.draw.DrawNextNum();
-            i1.checkNuminList(); */
             
-             /// .read access the asci value - then we write dline out so i can see 
-            Console.WriteLine(Console.ReadKey());
+           
+             
 
 
 
