cd3bf8db6d0525505dfab36a1ef4f903fb8780e1
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-06-16 14:10:10 +1000

    YESSS! upplimit cher is working - need fix msg

--- Draw.cs
+++ Draw.cs
@@ -78,23 +78,19 @@ namespace Bingo_Number_Generator
                 {
                     if (result > 0)
                     {
-                        Console.WriteLine(result + " .... really?" + " Only Numbers Please... try again");
+                        Console.WriteLine(result + " excellent choice" + result + " is a fine number");
                         goOn = true; /// so if the loop gets here it's passed the parse test.!-- We would then want the loop to exit
                         upperlimit = result;
                     }
-                    
+                    else // if it comes here that means that the result was not greater than 0... so a -1 etc... we cant have that so
+                    {
+                        Console.WriteLine ( result + " .... really?" + "Only above zero Numbers Please .. try again");
+                    }
 
                 }
-
-                if (result <= 0) /// not sure this is in the right spot.!--.!-- probs need to remove
-                {
-                    Console.WriteLine(result + " .... really?" + " Only above zero Numbers Please .. try again");
-                    continue;
-                }
-                
                 else
                 {
-                       Console.WriteLine("check the loop is doing the right thing here")
+                       Console.WriteLine("check the loop is doing the right thing here");
                 }
 
             }
